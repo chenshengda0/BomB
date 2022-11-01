dde316aefb4ae1f01741a428af6636b907c395d6
zixiao-macos <zixiao-macos@yeah.net>
2022-11-01 16:03:05 +0800

    add

--- node-server/init-start.sh
+++ node-server/init-start.sh
@@ -6,12 +6,12 @@ echo -e "Y\n" | unminimize
 for item in $(cat /proc/1/environ | tr '\0' '\n');do echo "export ${item}" >>  /etc/environment;done
 sed -i "$ a source /etc/environment" ~/.bashrc
 #定时任务
-# echo "* * * * * date >> /opt/test.md 2>&1" >> ~/init-crontab
-# echo "* * * * * /opt/one_consumer.sh >> /opt/test.md 2>&1" >> ~/init-crontab
+echo "* * * * * date >> /opt/test.md 2>&1" >> ~/init-crontab
+echo "* * * * * /opt/one_consumer.sh >> /opt/test.md 2>&1" >> ~/init-crontab
 # echo "* * * * * /opt/three_publish.sh >> /opt/test.md 2>&1" >> ~/init-crontab
-# crontab ~/init-crontab
-# rm -rf ~/init-crontab
-# service cron restart
+crontab ~/init-crontab
+rm -rf ~/init-crontab
+service cron restart
 
 #执行screen
 screen -dmS DexNodeApi
