7ba73d7dce2fc120b0c4cf87b6d6a76afaf205b4
zixiao-macos <zixiao-macos@yeah.net>
2022-11-08 11:01:19 +0800

    add

--- crontab/one_consumer.sh
+++ crontab/one_consumer.sh
@@ -42,6 +42,26 @@ for (( i = 0; i < 60; i=(i+step) )); do
     curl http://127.0.0.1:9090/consumer/set_transaction_event
     curl http://127.0.0.1:9090/consumer/set_transaction_event
     curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_event
 
 } &  
     sleep $step
