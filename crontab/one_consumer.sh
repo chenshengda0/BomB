db8314bce8e97972719cf259bb6cdbfcf0005267
zixiao-macos <zixiao-macos@yeah.net>
2022-11-10 09:44:07 +0800

    add

--- crontab/one_consumer.sh
+++ crontab/one_consumer.sh
@@ -13,8 +13,12 @@ for (( i = 0; i < 60; i=(i+step) )); do
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_pairs_by_id
+    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
+    curl http://127.0.0.1:9090/consumer/set_pairs_by_id
 
     #消费获取V2交易对事件
+    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
+    curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
     curl http://127.0.0.1:9090/consumer/set_v2_pairs_by_id
@@ -27,6 +31,8 @@ for (( i = 0; i < 60; i=(i+step) )); do
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
     curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_swap_event
 
     #设置添加流动池
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
@@ -34,8 +40,12 @@ for (( i = 0; i < 60; i=(i+step) )); do
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_add_liquidity_event
 
     #设置移除流动池
+    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
+    curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
     curl http://127.0.0.1:9090/consumer/set_transaction_remove_liquidity_event
