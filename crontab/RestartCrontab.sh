091140d5168370bc1ab3209c44b06bd76e3b2317
zixiao-macos <zixiao-macos@yeah.net>
2022-11-10 13:57:32 +0800

    add

--- crontab/RestartCrontab.sh
+++ crontab/RestartCrontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 screen -S NodeApi -X quit
 screen -dmS NodeApi
-screen -x -S NodeApi -p 0 -X stuff $"/usr/bin/node /opt/build/crontab.js >> /opt/build/crontab.md 2>&1"
+screen -x -S NodeApi -p 0 -X stuff $"/usr/bin/node /data/BomB/build/crontab.js >> /data/BomB/build/crontab.md 2>&1"
 screen -x -S NodeApi -p 0 -X stuff $"\n"
 echo "～服务已启动～"
